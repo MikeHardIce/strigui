284b737b0f0e1cf59417c19cb69b8e0d86672dea
OgreBelly <>
2020-08-06 00:24:06 +0100

    added button clicks, there is a weird glitch where another button draws its hover for a short moment

--- src/strigui/core.clj
+++ src/strigui/core.clj
@@ -1,12 +1,15 @@
 (ns strigui.core
-  (:require [clojure2d.core :as c2d]))
+  (:require [clojure2d.core :as c2d]
+            [clojure.set :as s]))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
-;;{:coord [] :func :args []}
+;;{:coord [] :func :args [] :name ""}
 (def buttons (atom []))
 
 (def buttons-to-redraw (atom #{}))
+
+(def buttons-clicked (atom #{}))
 
 (def canvas (c2d/canvas 600 600))
 
@@ -56,12 +59,12 @@
    y - y coordinate of top left corner
    color - vector consisting of [background-color font-color]
    min-width - the minimum width"
-  [canvas text {:keys [x y color min-width]}]
+  [canvas name text {:keys [x y color min-width]}]
   (let [args [canvas text {:x x :y y :color color :min-width min-width}]
         func create-button
         coord (apply func args)]
     (apply button-border (conj [canvas :black 1] coord))
-    (swap! buttons conj {:coord coord :func func :args args})))
+    (swap! buttons conj {:coord coord :func func :args args :name name})))
 
 (defn within? 
   "Checks wheter the point (x y) is within the given coord
@@ -88,23 +91,55 @@
 
 ;; create window
 (defn add-buttons [canvas]
-  (button canvas "Hello World!" {:x 50 :y 50 :color [:green :red]})
-  (button canvas "How are you?" {:x 50 :y 100 :color [:red :blue]})
-  (button canvas "Blah" {:x 50 :y 150 :color [:blue :yellow] :min-width 100})
-  (button canvas "Bye" {:x 50 :y 200 :color [:yellow :green] :min-width 100}))
+  (button canvas "a" "Hello World!" {:x 50 :y 50 :color [:green :red]})
+  (button canvas "b" "How are you?" {:x 50 :y 100 :color [:red :blue]})
+  (button canvas "c" "Blah" {:x 50 :y 150 :color [:blue :yellow] :min-width 100})
+  (button canvas "d" "Bye" {:x 50 :y 200 :color [:yellow :green] :min-width 100}))
+
+(defn- draw-hover 
+  "Draws the hover effect of the given button on the given canvas"
+  [canvas btn]
+  (when (not-empty btn)
+    (apply button-border (conj [canvas :black 2] (:coord btn)))
+    (swap! buttons-to-redraw #(set (conj %1 %2)) btn)))
+
+(defn- draw-clicked 
+  "Draws the click effect of the given button on the given canvas"
+  [canvas btn]
+  (when (not-empty btn)
+    (apply button-border (conj [canvas :green 2] (:coord btn)))
+    (swap! buttons-clicked  #(set (conj %1 %2)) btn)))
+
+(defn- redraw-button
+  "Redraws the default border of the given button on the given canvas" 
+  [canvas btn]
+  (let [coord (:coord btn)]
+    (when (not-empty coord) 
+            (apply button-border (conj [canvas :white 2] coord))
+            (apply button-border (conj [canvas :black 1] coord)))))
 
 (defmethod c2d/mouse-event ["main-window" :mouse-moved] [event state]
   (let [btn-hits (first (filter #(within? (:coord %) (c2d/mouse-x window) (c2d/mouse-y window)) @buttons))
         btns @buttons-to-redraw]
     (display-info canvas (str (c2d/mouse-pos window) " " @buttons-to-redraw))
     (if (empty? btn-hits)
-      (let [btn (first btns)
-            coord (:coord btn)]
-        (when (not-empty coord) 
-            (apply button-border (conj [canvas :white 2] coord))
-            (apply button-border (conj [canvas :black 1] coord))
-            (swap! buttons-to-redraw #(set (rest %2)) btns)))
-      (do 
-        (apply button-border (conj [canvas :black 2] (:coord btn-hits)))
-        (swap! buttons-to-redraw #(set (conj %1 %2)) btn-hits))))
+      (let [redrawn-buttons (map #(redraw-button canvas %) btns)]
+        (swap! buttons-to-redraw #(s/difference %1 (set %2))  redrawn-buttons))
+      (draw-hover canvas btn-hits)))
   state)
+
+(defmulti button-clicked :name)
+
+(defmethod button-clicked "" [])
+
+(defmethod c2d/mouse-event ["main-window" :mouse-pressed] [event state]
+  (let [btn (first (filter #(within? (:coord %) (c2d/mouse-x window) (c2d/mouse-y window)) @buttons))]
+    (draw-clicked canvas btn)
+    (button-clicked (:name btn)))
+  state)
+
+(defmethod c2d/mouse-event ["main-window" :mouse-released] [event state]
+  (let [btn (first @buttons-clicked)]
+    (draw-hover canvas btn))
+  state)
+
