21175264c2d321adf0be409f575325307a50e1f4
OgreBelly <ogrebelly@protonmail.com>
2020-12-26 19:18:10 +0000

    alpha3, updates license

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject strigui "0.0.1-alpha2"
+(defproject strigui "0.0.1-alpha3"
   :description "A simple GUI library."
   :url "https://github.com/OgreBelly/strigui"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT License"
+            :url "none"
+            :year 2020
+            :key "mit"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [clojure2d "1.3.1"]]
   :repl-options {:init-ns strigui.playground})
