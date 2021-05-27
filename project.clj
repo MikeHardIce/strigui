fc3c73e8bb6fe6e042d7b574cec3f8e8216d0d43
MikeHardIce <mikehardice@gmail.com>
2021-05-27 23:06:49 +0100

    alpha6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject strigui "0.0.1-alpha5"
+(defproject strigui "0.0.1-alpha6"
   :description "A small GUI library."
   :url "https://github.com/MikeHardIce/strigui"
   :license {:name "MIT License"
