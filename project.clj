939b35a9d0705715d857f04e8c0724a1d034508f
MikeHardIce <mikehardice@gmail.com>
2021-09-29 21:11:48 +0100

    alpha13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject strigui "0.0.1-alpha12"
+(defproject strigui "0.0.1-alpha13"
   :description "A small GUI library."
   :url "https://github.com/MikeHardIce/strigui"
   :license {:name "MIT License"
